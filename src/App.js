719f9c7016972f51984c1dfd75fd0b795cf0298e
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-26 16:06:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 function App() {
-  const comments = [
-    { id: 1, text: "Love this!" },
-    { id: 2, text: "Super good" },
-    { id: 3, text: "You are the best" },
-  ];
   return (
     <div className="container">
       <h1>Feedback App</h1>
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.text}</li>
-        ))}
-      </ul>
     </div>
   );
 }

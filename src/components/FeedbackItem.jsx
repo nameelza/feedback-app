ec4346c7844cf29858cf63a314a34af650eb98ea
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-28 15:27:25 -0700

    closeButton

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,24 @@
+import { FaTimes } from "react-icons/fa";
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
 
 function FeedbackItem({ item }) {
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={handleClick} className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
 
 FeedbackItem.propTypes = {
-  feedback: PropTypes.object.isRequired,
+  item: PropTypes.object.isRequired,
 };
 
 export default FeedbackItem;

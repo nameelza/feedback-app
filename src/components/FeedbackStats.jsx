ef8a67fa3fd52abd421f16f57a28ad0c2231e1e6
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-28 16:44:39 -0700

    Update FeedbackStats.jsx

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,5 +1,16 @@
-function FeedbackStats() {
-  return <div>Stats</div>;
+function FeedbackStats({ feedback }) {
+  // Calculate ratings average
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {average}</h4>
+    </div>
+  );
 }
 
 export default FeedbackStats;

b04ae7eca9af0da1695924728402c28d28802170
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-03 14:15:43 -0700

    Update FeedbackForm.jsx

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,6 +4,7 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {

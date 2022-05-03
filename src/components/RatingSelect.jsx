25518b9e7a9b7c3a524381d45797de68869b2371
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-03 16:00:49 -0700

    Update RatingSelect.jsx

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,8 +1,18 @@
 function RatingSelect() {
+  const handleChange = () => {
+    console.log("selected");
+  };
   return (
     <ul className="rating">
       <li>
-        <input type="radio" id="num1" name="rating" value="1" />
+        <input
+          type="radio"
+          id="num1"
+          name="rating"
+          value="1"
+          onChange={handleChange}
+          selected="true"
+        />
         <label htmlFor="num1">1</label>
       </li>
     </ul>

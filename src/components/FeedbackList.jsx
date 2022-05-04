aa6d31e3e43078ef32b45a4b155be2896c69ef7b
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-03 19:36:58 -0700

    framer-motion npm

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
@@ -7,13 +8,22 @@ function FeedbackList({ feedback, handleDelete }) {
       {!feedback || feedback.length === 0 ? (
         <div>No feedback available</div>
       ) : (
-        feedback.map((item) => (
-          <FeedbackItem
-            key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-          />
-        ))
+        <AnimatePresence>
+          {feedback.map((item) => (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ))}
+        </AnimatePresence>
       )}
     </div>
   );

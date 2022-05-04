7068e46521d673ffcd86708c3907423ca1c92059
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-04 10:13:25 -0700

    iconLink

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,5 @@
+function AboutIconLink() {
+    return <div>?</div>
+}
+
+export default AboutIconLink;
--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,7 +1,18 @@
 import Card from "./shared/Card";
 
 function AboutPage() {
-  return <Card>About</Card>;
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a react app to leave feedback about product or service</p>
+        <p>Version: 1.0.0</p>
+        <p>
+            <a href="/">Back To Home</a>
+        </p>
+      </div>
+    </Card>
+  );
 }
 
 export default AboutPage;

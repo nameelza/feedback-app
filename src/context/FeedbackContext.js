0d21ba1e077bc9ab41f70e7f8f7d8f4a81c25321
nameelza <82921161+nameelza@users.noreply.github.com>
2022-05-12 16:02:32 -0700

    FeedbackContext

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,8 +37,8 @@ export const FeedbackProvider = ({ children }) => {
 
   const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete this feedback?")) {
-      await fetch(`/feedback/${id}`, {method: "DELETE"})
-      
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -50,11 +50,19 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 

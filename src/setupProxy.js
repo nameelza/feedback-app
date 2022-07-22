d67b4d4fdd3b21e6705bbf40a56aab824faac5ac
nameelza <lizasvirschik@gmail.com>
2022-07-21 21:05:26 -0700

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/feedback", {
+    createProxyMiddleware("/feedback?_sort=id&_order=desc", {
       target: "http://localhost:5000",
     })
   );
